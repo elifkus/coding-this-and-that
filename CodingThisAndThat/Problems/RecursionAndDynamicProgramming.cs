2f886567697bc39407aadf5008270a549827f5ed
Elif <elifkus@gmail.com>
2015-05-17 15:39:25 +0000

    Calculated number of paths for robot

---
+++ CodingThisAndThat/Problems/RecursionAndDynamicProgramming.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems
+{
+    public class RecursionAndDynamicProgramming
+    {
+        /// <summary>
+        /// A robot sitting on the upper left corner of an X by Y grid. The robot can only
+        /// move in X and Y directions: right and down. This method calculates the number of 
+        /// possible paths the robot can go from (0,0) to (X,Y).
+        /// </summary>
+        public static int CalculatePossiblePathsForRobot(int x, int y)
+        { 
+            //X times in left, and Y times right
+            //All combinations as if each movement were unique
+            int allPaths = Factorial(x + y);
+
+            //There are only two types of movements.
+            allPaths = allPaths / x;
+            allPaths = allPaths / y;
+
+            return allPaths;
+        }
+
+        private static int Factorial(int number)
+        {
+            if (number == 0 || number == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return number * Factorial(number - 1);
+            }
+        }
+    }
+}
---
+++ CodingThisAndThat/ProblemsTest/RecursionAndDynamicProgrammingTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problems;
+
+namespace ProblemsTest
+{
+    [TestClass]
+    public class RecursionAndDynamicProgrammingTest
+    {
+        [TestMethod]
+        public void TestCalculatePossiblePathsForRobot()
+        {
+            int actual = RecursionAndDynamicProgramming.CalculatePossiblePathsForRobot(4, 4);
+            Assert.AreEqual(2520, actual);
+        }
+    }
+}
