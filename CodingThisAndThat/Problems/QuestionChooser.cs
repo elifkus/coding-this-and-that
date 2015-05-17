870278014099006271014a96240e4ffd3308f961
Elif <elifkus@gmail.com>
2015-05-17 10:59:00 +0000

    Changed test project type Added question chooser

---
+++ CodingThisAndThat/Problems/QuestionChooser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems
+{
+    public class QuestionChooser
+    {
+        Random random;
+        int[] numberOfQuestions;
+
+        public QuestionChooser()
+        {
+            this.random = new Random();
+            numberOfQuestions = new int[]{8, 7, 7, 9, 8, 6, 7, 10, 11, 7, 8, 6, 10, 6, 7, 6, 14, 13};
+        }
+
+        public string GiveMeAQuestion() {
+            int firstPart = random.Next(numberOfQuestions.Length);
+
+            int secondPart = random.Next(1, numberOfQuestions[firstPart]+1);
+            firstPart += 1;
+                
+            return firstPart.ToString() + "-" + secondPart.ToString();
+        }
+    }
+}
