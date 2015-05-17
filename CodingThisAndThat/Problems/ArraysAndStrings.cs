f523f6ff78d2dd5ee9f0dc594bd77ff06ecef7e3
Elif <elifkus@gmail.com>
2015-05-17 11:44:23 +0000

    Added basic compression problem

---
+++ CodingThisAndThat/Problems/ArraysAndStrings.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems
+{
+    public class ArraysAndStrings
+    {
+        public static string BasicallyCompress(string str) 
+        {
+            StringBuilder builder = new StringBuilder();
+            
+            char[] letters = str.ToCharArray();
+
+            int currenctCount = 0;
+            char previous = char.MaxValue;
+
+            for (int i = 0; i < letters.Length; i++)
+            {
+                if (i == 0) 
+                {
+                    previous = letters[i];
+                    currenctCount += 1;
+                    continue;
+                }
+
+                if (previous != letters[i])
+                {
+                    
+                    builder.Append(previous);
+                    builder.Append(currenctCount);
+
+                    previous = letters[i];
+                    currenctCount = 0;
+                }
+
+                currenctCount += 1;
+    
+            }
+
+            builder.Append(previous);
+            builder.Append(currenctCount);
+                
+            string compressedStr = builder.ToString();
+
+            if (compressedStr.Length < str.Length)
+            {
+                return compressedStr;
+            }
+            else 
+            {
+                return str;
+            }
+        }
+    }
+}
---
+++ CodingThisAndThat/ProblemsTest/ArraysAndStringsTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problems;
+
+namespace ProblemsTest
+{
+    [TestClass]
+    public class ArraysAndStringsTest
+    {
+        [TestMethod]
+        public void TestBasicallyCompress()
+        {
+            string str1 = "aabcccccaaa";
+            string expected1 = "a2b1c5a3";
+
+            string actual = ArraysAndStrings.BasicallyCompress(str1);
+            Assert.AreEqual(expected1, actual);
+
+            str1 = "abcdefg";
+            actual = ArraysAndStrings.BasicallyCompress(str1);
+            Assert.AreEqual(str1, actual);
+        }
+    }
+}
