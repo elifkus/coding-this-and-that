7be18ee88fd808e360ceb1fd5faa08d68038012f
Elif <elifkus@gmail.com>
2015-06-14 18:52:25 +0000

    Implemented find duplicate function

---
+++ CodingThisAndThat/Problems/ScalabilityAndMemoryLimits.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems
+{
+    public class ScalabilityAndMemoryLimits
+    {
+        public static void FindDuplicates(int[] numbers)
+        {
+            UInt32[] exists = new UInt32[1024];
+            int count = 0;
+            foreach (int number in numbers)
+            {
+                int i = number / 32;
+                int rem = ((int)number % 32);
+
+                if ((exists[i] & 1U << rem) > 0)
+                {
+                    Trace.WriteLine("Duplicate at "+count+": "  + number);
+                }
+                else
+                {
+                    exists[i] = exists[i] | 1U << rem;
+                }
+                count++;
+            }
+        }
+
+       
+
+    }
+}
---
+++ CodingThisAndThat/ProblemsTest/ScalabilityAndMemoryLimitsTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemsTest
+{
+    [TestClass]
+    public class ScalabilityAndMemoryLimitsTest
+    {
+        [TestMethod]
+        public void TestFindDuplicates()
+        {
+            int[] numbers = new int[500];
+            Random random = new Random();
+
+            for (int k = 0; k < 500; k++)
+            {
+                numbers[k] = random.Next(32001);
+            }
+
+            numbers[0] = numbers[100];
+
+            ScalabilityAndMemoryLimits.FindDuplicates(numbers);
+        }
+    }
+}
